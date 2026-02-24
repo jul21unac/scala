4a109723cdaf8a61f4865b180a99bf38438c3b22
jul21unac <jul21unac@gmail.com>
2026-02-24 22:52:20 +0100

    Add new class to celebrate the happy new year, add 2 method to get home apth and get cube of one number

---
+++ src/main/scala/HappyNewYear.scala
@@ -0,0 +1,6 @@
+class HappyNewYear (name: String, next_word: String) {
+
+  def ChineseYear(pref: String) : Unit =
+    println( pref + " Happy Chinese Year " + name + next_word)
+
+}
--- src/main/scala/ScalaBasic.scala
+++ src/main/scala/ScalaBasic.scala
@@ -1,3 +1,27 @@
 object ScalaBasic {
 
+  def main(args: Array[String]): Unit = {
+    println("========home============")
+
+    println(home)
+    print("=" * 50)
+    print("Cube")
+    print("=" * 50)
+    print("\n")
+    println(getCubeString(3))
+
+    print("=" * 50)
+    print("Happy New Year")
+    print("=" * 50)
+    print("\n")
+    val Happy = new HappyNewYear("Julio"," All the best for you")
+    Happy.ChineseYear("The best")
+  }
+
+  def home: String = System.getProperty("user.home")
+
+  def getCubeString(num: Double): String = {
+    val cube = num*num*num
+    cube.toString
+  }
 }
