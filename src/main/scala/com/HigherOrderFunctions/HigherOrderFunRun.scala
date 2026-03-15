428a98db7108fdbdbda36bffa29aae1a9f71461d
jul21unac <jul21unac@gmail.com>
2026-03-16 00:46:29 +0100

    HigherOrderFunctions practice 3

--- src/main/scala/com/HigherOrderFunctions/HigherOrderFunRun.scala
+++ src/main/scala/com/HigherOrderFunctions/HigherOrderFunRun.scala
@@ -9,9 +9,15 @@ object HigherOrderFunRun {
     pluralAnimalCurrying()
 
     val Bills = Seq(34545.55,2_00,1_00,3_000,3_1416,2_3321)
+    val Inc = List(45646.5,523423,34324,24234,324234)
 
     val CNS2E = new ConvertNuevosSoles2Euro(Bills,4.13)
     println(CNS2E.changeNuevosSoles2Euro)
+
+    println(Expenses.taxes(Inc))
+    println(Expenses.MobilBill(Inc))
+    println(Expenses.EnergyBill(Inc))
+
 
 
   }
