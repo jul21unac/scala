ad6e46bf1ad1d5f5421c929719f35d93712b2427
jul21unac <jul21unac@gmail.com>
2026-03-17 16:59:39 +0100

    docker and hi

--- src/main/scala/com/HigherOrderFunctions/HelloFuntion.scala
+++ src/main/scala/com/HigherOrderFunctions/HelloFuntion.scala
@@ -3,6 +3,6 @@ package com.HigherOrderFunctions
 object HelloFuntion {
   def SayHi(HiStr: String, people: Boolean): (String, String) => String = {
     val cadStr = if (people) "People" else "You"
-    (Start: String, end: String) => s"$Sart $HiStr $end"
+    (Start: String, end: String) => s"$Start $HiStr $end"
   }
 }
