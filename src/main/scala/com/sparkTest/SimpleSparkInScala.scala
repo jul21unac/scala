71ef5229d8643d64f2a0727d57821ddc141bd560
jul21unac <jul21unac@gmail.com>
2026-03-20 21:50:09 +0100

    connect impala with hive external

--- src/main/scala/com/sparkTest/SimpleSparkInScala.scala
+++ src/main/scala/com/sparkTest/SimpleSparkInScala.scala
@@ -24,27 +24,31 @@ object SimpleSparkInScala {
   }
 
   def testHive(): Unit = {
-    val spark = SparkSession.builder()
+/*    val spark = SparkSession.builder()
       .appName("HiveTableExample")
       .config("hive.metastore.uris", "thrift://localhost:9083")  // O host.docker.internal si Spark en Docker
-      .config("spark.sql.warehouse.dir", "/user/hive/warehouse")
+      .config("spark.sql.warehouse.dir", "/opt/hive/data/warehouse")
       .enableHiveSupport()
       .getOrCreate()
-
-    /*
+*/
+val spark = SparkSession.builder()
+  .appName("HiveTableExample")
+  .config("hive.metastore.uris", "thrift://localhost:9083")
+  .enableHiveSupport()  // sin .config("spark.sql.warehouse.dir", ...)
+  .getOrCreate()
+ 
     spark.sql("""
-  CREATE TABLE IF NOT EXISTS mi_tabla (
+  CREATE TABLE IF NOT EXISTS mi_tabla_5 (
     id INT,
     nombre STRING
   ) USING hive
 """)
-*/
 
     spark.sql("""
-  insert into mi_tabla values (1, 'lalal')
+  insert into mi_tabla_5 values (2, 'lalal')
 """)
 
-val resultado = spark.sql("SELECT * FROM mi_tabla")
+val resultado = spark.sql("SELECT * FROM mi_tabla_5")
 resultado.show()  // Muestra todas las filas (20 por defecto)
 
 spark.stop()
