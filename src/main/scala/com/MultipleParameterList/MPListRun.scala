cf267b01dbfd35ea346ee74d6ac061f0bddbdbd9
jul21unac <jul21unac@gmail.com>
2026-03-27 19:29:34 +0100

    first method

--- src/main/scala/com/MultipleParameterList/MPListRun.scala
+++ src/main/scala/com/MultipleParameterList/MPListRun.scala
@@ -3,12 +3,18 @@ package com.MultipleParameterList
 object MPListRun {
   
     def main(args: Array[String]): Unit = {
-
+            house()
     }
 
     def house()
     {
-        
+        val words = List("c", "o", "n", "g", "r", "a", "t", "u", "l", "a", "t", "i", "o", "n", "s")
+        val res = words.foldLeft("")((m, n) => m + n)
+        println(res)
+
+        val word_2 = List("w", "e", "l", "c", "o", "m", "e")
+        val res_2 = word_2.foldLeft("")((m, n) => m + n)
+        println(res_2)
     }
 
 }
