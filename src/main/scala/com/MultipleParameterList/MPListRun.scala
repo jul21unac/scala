e79aa6dc78ea0ff30f7d9c70ef4e0f91c874f596
jul21unac <jul21unac@gmail.com>
2026-03-29 13:14:21 +0200

    add new function

--- src/main/scala/com/MultipleParameterList/MPListRun.scala
+++ src/main/scala/com/MultipleParameterList/MPListRun.scala
@@ -7,6 +7,10 @@ object MPListRun {
             val words_3 = List("h","o","m","e")
             val a= foldLeft1[String,String](words_3, "",(m, n) => m + n)
             println(a)
+
+            val str_2 = List("cook","ingenue" ,"reading","writing","swimming","running","playing","eat","clean","car")
+            val filtered = filter(str_2, haveIng)
+            println(filtered)
     }
 
     def house()
@@ -24,8 +28,18 @@ object MPListRun {
     }
    
     def foldLeft1[A, B](as: List[A], b0: B, op: (B, A) => B) : B =
-  if (as.isEmpty) b0
-  else foldLeft1(as.tail, op(b0, as.head), op)
+      if (as.isEmpty) b0
+      else foldLeft1(as.tail, op(b0, as.head), op)
 
+    def filter(str_1 : List[String], f : String => Boolean) : List[String] =     
+        if (str_1.isEmpty) str_1
+        else if (f( str_1.head) ) str_1.head :: filter(str_1.tail, f)
+        else filter(str_1.tail, f)
+      
+    def haveIng(str_2 : String) : Boolean = 
+        if (str_2.endsWith("ing")) true
+        else false
+
+      
 
 }
