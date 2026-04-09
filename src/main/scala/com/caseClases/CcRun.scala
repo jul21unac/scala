974a75c34ec9ffc771ffc34e433fbb54c83b60dc
jul21unac <jul21unac@gmail.com>
2026-04-09 22:49:30 +0200

    another disco

--- src/main/scala/com/caseClases/CcRun.scala
+++ src/main/scala/com/caseClases/CcRun.scala
@@ -5,7 +5,10 @@ object CcRun {
     def main(args: Array[String]): Unit = {
         
         val discotec = Discotec("salsa")
-        println(discotec.music)
+        val discotec3 = Discotec("Rock")
+        println(discotec.music) 
+        println(discotec3.music)
+
     }
 }
 
