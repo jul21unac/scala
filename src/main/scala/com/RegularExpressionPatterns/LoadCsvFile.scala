73b5044837ac5d68e4804ea3a3335e6dac656245
jul21unac <jul21unac@gmail.com>
2026-05-25 23:07:59 +0200

    try regex

--- src/main/scala/com/RegularExpressionPatterns/LoadCsvFile.scala
+++ src/main/scala/com/RegularExpressionPatterns/LoadCsvFile.scala
@@ -16,7 +16,11 @@ class LoadCsvFile {
             val fields = line.split(",").map(_.trim)
             //println(fields.mkString(", "))
              i=i+1
-            println( s"Line $i: $line" )
+             val regex = """([A-Za-z0-9]+),([A-Za-z0-9]+),([0-9]+)""".r
+             line match {
+                 case regex(review_id,order_id,review_score) => println(s"Line $i: Review ID=$review_id, Order ID=$order_id, Review Score=$review_score")
+                 case _ => println(s"Line $i: $line")
+             }
         }
         
     }
