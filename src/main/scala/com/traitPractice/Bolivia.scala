5b1d06d5ae8af94ef8790230c0a6235c79d6c45c
jul21unac <jul21unac@gmail.com>
2026-08-08 11:43:47 +0200

    add Run objecto for traits and start adapting the class Bolivia

--- src/main/scala/com/traitPractice/Bolivia.scala
+++ src/main/scala/com/traitPractice/Bolivia.scala
@@ -2,13 +2,20 @@ package com.traitPractice
 
 class Bolivia extends Country {
 
-  val Capital: String = ???
+  val Capital: String = "La Paz"
 
-  val budget: Integer = ???
+  val budget: Integer = 17000
+
+  val costOfUniversity:Int = 500
+
+  val costOfSchools:Int=  400
+
+  val costOfHospitals:Int = 800
 
   def createUniversity(qty: Integer, UniBudget: Integer): Unit = {
 
 
+      println(calculateValidate(qty,UniBudget,"University"))
 
   }
 
@@ -17,6 +24,19 @@ class Bolivia extends Country {
   }
 
   def createHospitals(qty: Integer, HealtBudget: Integer): Unit = {
+
+  }
+  //TODO : adapt for all the methods
+  //TODO: acumulate the costs
+  def calculateValidate(qt:Integer, partialBudget:Integer, Orga : String): String= {
+    val messageV : String =
+    if (partialBudget > budget) {
+          "We don't have that budget for "+ Orga
+    }  else if( qt*costOfUniversity > budget)
+      {  "Reduce the quantity of "+ Orga  }
+    else
+      { s"Proceed to create  $qt of $Orga" }
+    messageV
 
   }
 }
---
+++ src/main/scala/com/traitPractice/traitExampleRun.scala
@@ -0,0 +1,16 @@
+package com.traitPractice
+
+object traitExampleRun {
+
+  def main(args: Array[String]): Unit ={
+
+    val Bol = new Bolivia()
+
+    Bol.createUniversity(2,800)
+
+
+
+
+  }
+
+}
