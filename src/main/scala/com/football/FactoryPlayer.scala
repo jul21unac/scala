6d898fb5a4cd1fd3fcda82c7c5ac7fb513c29ce7
jul21unac <jul21unac@gmail.com>
2026-09-06 07:50:52 +0200

    add fact

--- src/main/scala/com/football/FactoryPlayer.scala
+++ src/main/scala/com/football/FactoryPlayer.scala
@@ -1,5 +1,19 @@
 package com.football
 
 object FactoryPlayer {
-  
+
+  def NewPlayer(typePlayer: String): Player = typePlayer.toLowerCase match {
+
+    case "defence" => new Defence()
+    case "attack"  => new Attack()
+    case _         => throw new IllegalArgumentException("Unknown Player type")
+
+  }
+
+  def getInt(rangI: Int, rangF: Int): Int = {
+    val rand = new scala.util.Random
+    rand.between(rangI, rangF)
+
+  }
+
 }
--- src/main/scala/com/football/FutbolData.scala
+++ src/main/scala/com/football/FutbolData.scala
@@ -1,5 +1,80 @@
 package com.football
 
 object FutbolData {
-  
+
+  val DefensePlayers: List[(String, String)] = List(
+    // FC BARCELONA
+    ("Ronald Araújo", "FC Barcelona"),
+    ("Jules Koundé", "FC Barcelona"),
+    ("Pau Cubarsí", "FC Barcelona"),
+    ("Alejandro Balde", "FC Barcelona"),
+    ("Eric García", "FC Barcelona"),
+    ("Andreas Christensen", "FC Barcelona"),
+    ("Gerard Martín", "FC Barcelona"),
+    ("Héctor Fort", "FC Barcelona"),
+    ("Joao Cancelo", "FC Barcelona"),
+
+    // REAL MADRID
+    ("Antonio Rüdiger", "Real Madrid"),
+    ("Éder Militão", "Real Madrid"),
+    ("Dani Carvajal", "Real Madrid"),
+    ("Ferland Mendy", "Real Madrid"),
+    ("David Alaba", "Real Madrid"),
+
+    // ATLÉTICO DE MADRID
+    ("Marcos Llorente", "Atlético de Madrid"),
+    ("Marc Pubill", "Atlético de Madrid"),
+    ("José María Giménez", "Atlético de Madrid"),
+    ("Robin Le Normand", "Atlético de Madrid"),
+    ("Cuti Romero", "Atlético de Madrid"),
+    ("Dávid Hancko", "Atlético de Madrid"),
+    ("Alejandro Grimaldo", "Atlético de Madrid"),
+
+    // ATHLETIC CLUB
+    ("Dani Vivian", "Athletic Club"),
+    ("Yeray Álvarez", "Athletic Club"),
+    ("Aymeric Laporte", "Athletic Club"),
+    ("Yuri Berchiche", "Athletic Club"),
+    ("Jesús Areso", "Athletic Club"),
+    ("Aitor Paredes", "Athletic Club"),
+    ("Andoni Gorosabel", "Athletic Club"),
+
+    // REAL SOCIEDAD
+    ("Jon Aramburu", "Real Sociedad"),
+    ("Aritz Elustondo", "Real Sociedad"),
+    ("Igor Zubeldia", "Real Sociedad"),
+    ("Javi Galán", "Real Sociedad"),
+
+    // REAL BETIS
+    ("Marc Bartra", "Real Betis"),
+    ("Fran García", "Real Betis"),
+    ("Héctor Bellerín", "Real Betis"),
+    ("Junior Firpo", "Real Betis"),
+    ("Natan", "Real Betis"),
+    ("Diego Llorente", "Real Betis"),
+
+    // SEVILLA FC
+    ("Loïc Badé", "Sevilla FC"),
+    ("Nemanja Gudelj", "Sevilla FC"),
+    ("Kike Salas", "Sevilla FC"),
+    ("José Ángel Carmona", "Sevilla FC"),
+    ("Adrià Pedrosa", "Sevilla FC"),
+
+    // VILLARREAL CF
+    ("Kiko Femenía", "Villarreal CF"),
+    ("Raúl Albiol", "Villarreal CF"),
+    ("Eric Bailly", "Villarreal CF"),
+
+    // VALENCIA CF
+    ("Cristhian Mosquera", "Valencia CF"),
+    ("José Gayà", "Valencia CF"),
+    ("Mouctar Diakhaby", "Valencia CF"),
+
+    // GETAFE CF
+    ("Omar Alderete", "Getafe CF"),
+    ("Juan Iglesias", "Getafe CF"),
+    ("Diego Rico", "Getafe CF"),
+    ("Domingos Duarte", "Getafe CF")
+  )
+
 }
