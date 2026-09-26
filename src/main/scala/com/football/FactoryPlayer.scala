eca44b816f451fe34d48e88d125614948439d297
Julio Cesar Valdivia Marin <jul21unac@gmail.com>
2026-09-26 12:26:32 +0200

    Feature/57 create random football 11 team (#62)
    * change buffer list
    
    * add random creation
    
    * add team random
    
    * change team inside clases
    
    * fix problems to run add toString
    
    * create def team
    
    * fix duplicated player

--- src/main/scala/com/football/FactoryPlayer.scala
+++ src/main/scala/com/football/FactoryPlayer.scala
@@ -1,72 +1,90 @@
 package com.football
 import scala.util.Random
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
 
 object FactoryPlayer {
 
-  val d = 0
   val teams = createTeams
 
-  def NewPlayer(typePlayer: String): Player = {
+  def NewPlayer(typePlayer: String, usedNames: ListBuffer[String]): Player = {
 
-    val RealMadrid = findTeam("Real Madrid", teams)
-    // new Team(name = "Real Madrid", league = "La Liga", "Spain", 433)
+    var defensePlay= FootbalData.DefensePlayers
+
+    var attackPlay = FootbalData.attack
+
+    var goolKeeper = FootbalData.goalkeeper
+
+    var midFieldPla = FootbalData.midFieldPlayer
+
+    val d = getInt(0, defensePlay.size-1)
+    val a = getInt(0, attackPlay.size-1)
+    val g = getInt(0, goolKeeper.size-1)
+    val m = getInt(0, midFieldPla.size-1)
 
     typePlayer.toLowerCase match {
 
       case "defence" =>
+      val (name, team) =
+        chooseAvailable(FootbalData.DefensePlayers, usedNames)        
         new Defence(
-          FootbalData.DefensePlayers(d)._1,
+          name,
           getInt(17, 45),
           "Defence",
           getInt(50, 100),
           getInt(0, 50),
           getInt(0, 100),
           getInt(0, 100),
-          RealMadrid,
+          findTeam(team, teams),
           getInt(0, 100),
           getInt(0, 100),
           getInt(0, 100),
           getInt(0, 100)
         )
       case "attack" =>
+      val (name, team) =
+        chooseAvailable(FootbalData.attack, usedNames)          
         new Attack(
-          FootbalData.attack(d)._1,
+          name,
           getInt(17, 45),
           "Attack",
           getInt(50, 100),
           getInt(0, 50),
           getInt(0, 100),
           getInt(0, 100),
-          RealMadrid,
+          findTeam(team, teams),
           getInt(0, 100),
           getInt(0, 100)
         )
       case "midfield" =>
+      val (name, team) =
+        chooseAvailable(FootbalData.midFieldPlayer, usedNames)          
         new Midfield(
-          FootbalData.midFieldPlayer(d)._1,
+          name,
           getInt(17, 45),
-          "Attack",
+          "Midfield",
           getInt(50, 100),
           getInt(0, 50),
           getInt(0, 100),
           getInt(0, 100),
-          RealMadrid,
+          findTeam(team, teams),
           getInt(0, 100),
           getInt(0, 100),
           getInt(0, 100),
           getInt(0, 100)
         )
       case "goalkeeper" =>
+      val (name, team) =
+        chooseAvailable(FootbalData.goalkeeper, usedNames)          
         new GoalKeeper(
-          FootbalData.goalkeeper(d)._1,
+          name,
           getInt(17, 45),
-          "Attack",
+          "GoalKeeper",
           getInt(50, 100),
           getInt(0, 50),
           getInt(0, 100),
           getInt(0, 100),
-          RealMadrid,
+          findTeam(team, teams),
           getInt(0, 100),
           getInt(0, 100),
           getInt(0, 100),
@@ -94,11 +112,25 @@ object FactoryPlayer {
   }
 
   def findTeam(teamName: String, teams: ArrayBuffer[(Team)]): Team = {
-
+    
     val teamf = teams.filter(_.name.equalsIgnoreCase(teamName))
 
+    if (teamf.size == 0 ) 
+      {
+        println(teamName)
+        throw new Exception(teamName)
+      }
     teamf(0)
+   
 
   }
+  def chooseAvailable(   playe: Seq[(String, String)],   usedName: ListBuffer[String] ): (String, String) = {
+  val disponibles = playe.filterNot {
+    case (playe, _) => usedName.contains(playe)
+  }
+
+  require(disponibles.nonEmpty, "no players")
+  disponibles(Random.nextInt(disponibles.size))
+}
 
 }
--- src/main/scala/com/football/RunFootbal.scala
+++ src/main/scala/com/football/RunFootbal.scala
@@ -1,27 +1,38 @@
 package com.football
 
+import scala.collection.mutable.ListBuffer
+
 object RunFootbal {
 
   def main(arg: Array[String])
   {
 
-    val defen_2 = FactoryPlayer.NewPlayer("Defence")
+    val players = createTeam(Array(3,5,2),"Random FC")
 
-    val attPla = FactoryPlayer.NewPlayer("attack")
-
-    val team1 = FactoryTeam.NewTeam("Barza")
-
-    val miedPla = FactoryPlayer.NewPlayer("midField")
-
-    val goalKPla = FactoryPlayer.NewPlayer("GoalKeeper")
-
-    // println(Defen_1.Name)
-    println(defen_2.Name)
-    println(attPla.Name)
-    println(team1.name + team1.Strategy)
-    println(miedPla.Name)
-    println(goalKPla)
+    players.foreach(println)
 
   }
+
+def createTeam(tactic: Array[Int], nombre: String ) : ListBuffer[Player] =
+{
+  if (tactic.length != 3) throw new Exception
+    val players = ListBuffer.empty[Player]
+    var usedNames = ListBuffer.empty[String]
+
+    def addPlayer(position: String): Unit = {
+      val player = FactoryPlayer.NewPlayer(position, usedNames)
+      players += player
+      usedNames += player.Name
+    }
+
+    addPlayer("GoalKeeper")
+
+
+
+    for (_ <- 0 until tactic(0)) addPlayer("Defence")
+    for (_ <- 0 until tactic(1)) addPlayer("Midfield")
+    for (_ <- 0 until tactic(2)) addPlayer("Attack")
+  players
+}
 
 }
